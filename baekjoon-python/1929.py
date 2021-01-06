a26d7ae22824445f03bbaf40fa1225daa4936e24
JerryK026 <kskyung0624@gmail.com>
2021-01-07 00:14:58 +0900

    clear

--- baekjoon-python/1929.py
+++ baekjoon-python/1929.py
@@ -2,22 +2,22 @@ import sys
 
 
 def run():
-    line = sys.stdin.readline().split()
-    M, N = [int(x) for x in line]
-    eratos, result = list(), list()
+    M, N = sys.stdin.readline().split()
+    M, N = int(M), int(N) + 1
 
-    for i in range(2, N + 1):
-        eratos.append(i)
+    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+    eratos = [True] * N
+    eratos[0], eratos[1] = False, False
 
-    for i in eratos:
-        if i >= M:
-            result.append(i)
-        tmp = N // i
-        tmp2 = i
-        for j in range(1, tmp):
-            tmp2 += i
-            if tmp2 in eratos:
-                eratos.remove(tmp2)
+    # n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사
+    m = int(N ** 0.5)
+    for i in range(2, m + 1):
+        if eratos[i] == True:           # i가 소수인 경우
+            for j in range(i+i, N, i): # i이후 i의 배수들을 False 판정
+                eratos[j] = False
+
+    # 소수 목록 산출
+    result = [i for i in range(M, N) if eratos[i] == True]
 
     for i in result:
         print(i)
