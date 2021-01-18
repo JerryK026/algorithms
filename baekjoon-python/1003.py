1991f485aba4855c315c4af46ed904417a136fae
JerryK026 <kskyung0624@gmail.com>
2021-01-18 21:16:17 +0900

    clear

--- baekjoon-python/1003.py
+++ baekjoon-python/1003.py
@@ -1,24 +1,14 @@
 import sys
 
-arr = [0, 0]
-
 
 def run():
+    arr = [[1, 0], [0, 1]]
+    for _ in range(2, 41):
+        arr.append([arr[-1][i] + arr[-2][i] for i in range(2)])
+
     for _ in range(int(sys.stdin.readline())):
-        fibonacci(int(sys.stdin.readline()))
-        print(arr[0], arr[1], sep=" ")
-        arr[0], arr[1] = [0, 0]
-
-
-def fibonacci(num):
-    if num == 0:
-        arr[0] += 1
-        return 0
-    elif num == 1:
-        arr[1] += 1
-        return 1
-    else:
-        return fibonacci(num - 1) + fibonacci(num - 2)
+        a, b = arr[int(sys.stdin.readline())]
+        print(a, b)
 
 
 if __name__ == "__main__":
